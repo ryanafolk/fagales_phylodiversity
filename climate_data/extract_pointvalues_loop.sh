d23ece93666c029ccd777c715c9c18614a1c5cbd
Ryan A. Folk <folk.41@buckeyemail.osu.edu>
2023-04-17 16:14:20 -0500

    clean up repository

--- climate_data/extract_pointvalues_loop.sh
+++
@@ -1,5 +0,0 @@
-# This one drops only the missing values for each point, so point associations are lost
-for i in /mnt/Botbot/nitfix/Saxifragales_all_layers_30s/*.asc; do
-python3 extract_pointvalues_spatialgrid.py ${i} ./../Fagales_CSVs_ToShare/
-done
-
